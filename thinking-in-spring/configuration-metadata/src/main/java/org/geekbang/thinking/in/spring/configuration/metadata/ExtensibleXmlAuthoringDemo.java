6f3c27547572b87e0ff0467aadecf53cc21fb9f9
mercyblitz <mercyblitz@gmail.com>
2020-01-12 17:06:20 +0800

    Polish code for Chapter 10

---
+++ thinking-in-spring/configuration-metadata/src/main/java/org/geekbang/thinking/in/spring/configuration/metadata/ExtensibleXmlAuthoringDemo.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geekbang.thinking.in.spring.configuration.metadata;
+
+import org.geekbang.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+/**
+ * Spring XML 元素扩展示例
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since
+ */
+public class ExtensibleXmlAuthoringDemo {
+
+    public static void main(String[] args) {
+
+        // 创建 IoC 底层容器
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        // 创建 XML 资源的 BeanDefinitionReader
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        // 记载 XML 资源
+        reader.loadBeanDefinitions("META-INF/users-context.xml");
+        // 获取 User Bean 对象
+        User user = beanFactory.getBean(User.class);
+        System.out.println(user);
+    }
+}
