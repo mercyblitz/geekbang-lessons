6b8a3ba8c5858b8122e758119e5bd43f0b923877
liubeicai@linklogis.com <xiaoheitalk567@163.com>
2020-06-11 23:57:14 +0800

    modify

---
+++ thinking-in-spring/bean-lifecycle/src/test/java/Test.java
@@ -0,0 +1,13 @@
+import org.springframework.beans.factory.BeanFactory;
+
+/**
+ * @author xiaoheitalk
+ * @type Test
+ * @date 2020-6-11 23:40
+ */
+public class Test {
+    public static void main(String[] args) {
+        Object a = "Test";
+        System.out.println(a instanceof Class);
+    }
+}
