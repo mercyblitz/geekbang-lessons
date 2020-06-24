e8630ede6af6be63f711d4ae83b894e5375d93b4
xiaoheitalk <xiaoheitalk567@163.com>
2020-06-24 17:39:09 +0800

    modify

---
+++ thinking-in-spring/configuration-metadata/src/main/java/org/geekbang/thinking/in/spring/configuration/metadata/xml/XmlBeanDefinitionReaderDemo.java
@@ -0,0 +1,12 @@
+package org.geekbang.thinking.in.spring.configuration.metadata.xml;
+
+/**
+ * @author xiaoheitalk
+ * @type XmlBeanDefinitionReaderDemo
+ * @date 2020/6/19 12:44
+ */
+public class XmlBeanDefinitionReaderDemo {
+    public static void main(String[] args) {
+
+    }
+}
