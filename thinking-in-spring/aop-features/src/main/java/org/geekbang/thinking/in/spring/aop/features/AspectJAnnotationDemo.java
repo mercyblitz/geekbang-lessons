3edf0bd64ce1d20f5c18875b9897fa12782fb5e9
mercyblitz <mercyblitz@gmail.com>
2020-11-24 20:02:41 +0800

    Polish code for Spring AOP chapter I & II

---
+++ thinking-in-spring/aop-features/src/main/java/org/geekbang/thinking/in/spring/aop/features/AspectJAnnotationDemo.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geekbang.thinking.in.spring.aop.features;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since
+ */
+@Aspect        // 声明为 Aspect 切面
+@Configuration // Configuration class
+@EnableAspectJAutoProxy // 激活 Aspect 注解自动代理
+public class AspectJAnnotationDemo {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AspectJAnnotationDemo.class);
+        context.refresh();
+
+        AspectJAnnotationDemo aspectJAnnotationDemo = context.getBean(AspectJAnnotationDemo.class);
+
+        context.close();
+    }
+}
---
+++ thinking-in-spring/aop-features/src/main/java/org/geekbang/thinking/in/spring/aop/features/AspectJXmlDemo.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geekbang.thinking.in.spring.aop.features;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.geekbang.thinking.in.spring.aop.overview.EchoService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since
+ */
+@Aspect        // 声明为 Aspect 切面
+@Configuration // Configuration class
+public class AspectJXmlDemo {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("classpath:/META-INF/spring-aop-context.xml");
+
+//        AspectJXmlDemo aspectJAnnotationDemo = context.getBean(AspectJXmlDemo.class);
+
+        context.close();
+
+    }
+}
