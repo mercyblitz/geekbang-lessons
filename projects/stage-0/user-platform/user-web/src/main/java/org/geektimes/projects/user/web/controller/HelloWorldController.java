6f8fe49961bbd548d8fb68c32a942ab454cd542b
liuawei <liuawei@lerke.cn>
2021-03-15 14:51:06 +0800

    Update HelloWorldController.java

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/HelloWorldController.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/HelloWorldController.java
@@ -1,12 +1,12 @@
 package org.geektimes.projects.user.web.controller;
 
-import org.geektimes.web.mvc.controller.PageController;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+
+import org.geektimes.web.mvc.controller.PageController;
 
 /**
  * 输出 “Hello,World” Controller
