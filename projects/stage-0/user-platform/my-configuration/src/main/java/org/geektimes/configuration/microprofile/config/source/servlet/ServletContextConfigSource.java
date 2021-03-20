476b50a0ec4c0fef0a2d69ee19f3195af7a43afb
Mercy Ma <mercyblitz@gmail.com>
2021-03-20 22:17:59 +0800

    Polish code for stage-0's project

---
+++ projects/stage-0/user-platform/my-configuration/src/main/java/org/geektimes/configuration/microprofile/config/source/servlet/ServletContextConfigSource.java
@@ -0,0 +1,26 @@
+package org.geektimes.configuration.microprofile.config.source.servlet;
+
+import org.geektimes.configuration.microprofile.config.source.MapBasedConfigSource;
+
+import javax.servlet.ServletContext;
+import java.util.Enumeration;
+import java.util.Map;
+
+public class ServletContextConfigSource extends MapBasedConfigSource {
+
+    private final ServletContext servletContext;
+
+    public ServletContextConfigSource(ServletContext servletContext) {
+        super("ServletContext Init Parameters", 500);
+        this.servletContext = servletContext;
+    }
+
+    @Override
+    protected void prepareConfigData(Map configData) throws Throwable {
+        Enumeration<String> parameterNames = servletContext.getInitParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String parameterName = parameterNames.nextElement();
+            configData.put(parameterName, servletContext.getInitParameter(parameterName));
+        }
+    }
+}
