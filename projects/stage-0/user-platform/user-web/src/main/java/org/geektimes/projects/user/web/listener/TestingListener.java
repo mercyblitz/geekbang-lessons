58f377561db04438d4f46b3141b891677186fe4d
mercyblitz <mercyblitz@gmail.com>
2021-03-27 00:57:50 +0800

    Polish Code

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/TestingListener.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/TestingListener.java
@@ -4,6 +4,7 @@ import org.geektimes.context.ClassicComponentContext;
 import org.geektimes.projects.user.domain.User;
 import org.geektimes.projects.user.sql.DBConnectionManager;
 
+import javax.jms.*;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.servlet.ServletContext;
@@ -30,6 +31,9 @@ public class TestingListener implements ServletContextListener {
         logger.info("所有的 JNDI 组件名称：[");
         context.getComponentNames().forEach(logger::info);
         logger.info("]");
+
+        ConnectionFactory connectionFactory = context.getComponent("jms/activemq-factory");
+        testJms(connectionFactory);
     }
 
     private void testPropertyFromServletContext(ServletContext servletContext) {
@@ -54,6 +58,48 @@ public class TestingListener implements ServletContextListener {
         transaction.begin();
         entityManager.persist(user);
         transaction.commit();
+    }
+
+    private void testJms(ConnectionFactory connectionFactory) {
+
+        try {
+            // Create a Connection
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            // Create a Session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create the destination (Topic or Queue)
+            Destination destination = session.createQueue("TEST.FOO");
+
+            // Create a MessageProducer from the Session to the Topic or Queue
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            // Create a messages
+            String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
+            TextMessage message = session.createTextMessage(text);
+
+            // Tell the producer to send the message
+            System.out.println("Sent message: " + message.hashCode() + " : " + Thread.currentThread().getName());
+            producer.send(message);
+
+            // Create a MessageConsumer from the Session to the Topic or Queue
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            // Wait for a message
+            message = (TextMessage) consumer.receive(1000);
+
+            System.out.println("Received: " + message.getText());
+
+            // Clean up
+            session.close();
+            connection.close();
+        } catch (JMSException e) {
+
+        }
+
     }
 
     @Override
