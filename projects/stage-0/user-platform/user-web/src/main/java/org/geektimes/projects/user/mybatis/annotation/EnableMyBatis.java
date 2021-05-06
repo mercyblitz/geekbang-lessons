bdbd874f29c7ef377c419a104a7b66340539bd56
Mercy Ma <mercyblitz@gmail.com>
2021-05-06 22:28:30 +0800

    Polish code

---
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/mybatis/annotation/EnableMyBatis.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geektimes.projects.user.mybatis.annotation;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * 激活 MyBatis
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since TODO
+ * Date : 2021-05-06
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Inherited
+@Import(MyBatisBeanDefinitionRegistrar.class)
+public @interface EnableMyBatis {
+
+    /**
+     * @return the bean name of {@link SqlSessionFactoryBean}
+     */
+    String value() default "sqlSessionFactoryBean";
+
+    /**
+     * @return DataSource bean name
+     */
+    String dataSource();
+
+    /**
+     * the location of {@link Configuration}
+     *
+     * @return
+     */
+    String configLocation();
+
+    /**
+     * @return the location of {@link Mapper}
+     * @see MapperScan
+     */
+    String[] mapperLocations() default {};
+
+    String environment() default "SqlSessionFactoryBean";
+
+}
