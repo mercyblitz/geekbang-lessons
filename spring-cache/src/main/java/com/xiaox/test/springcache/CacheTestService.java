14add7e1aed59a0cd9311e067c5785bad9920862
xiaoxiong <306505224@qq.com>
2021-05-13 16:43:53 +0800

    提交自定义Cache和CacheManager。适配4大缓存操作注解

---
+++ spring-cache/src/main/java/com/xiaox/test/springcache/CacheTestService.java
@@ -0,0 +1,21 @@
+package com.xiaox.test.springcache;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Classname CacheTestService
+ * @Description TODO
+ * @Date 2021/5/13 3:33 下午
+ * @Author by xiaoxiong
+ */
+@Service
+public class CacheTestService {
+
+    @Cacheable(cacheNames = "test1",key = "#a",cacheManager="CustomRedisCacheManager")
+    public String test(String a){
+       System.out.println("进入方法执行！！！！传入参数："+a);
+       return "Hello world";
+    }
+
+}
