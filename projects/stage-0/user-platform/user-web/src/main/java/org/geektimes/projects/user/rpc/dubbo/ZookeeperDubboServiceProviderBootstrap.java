20b80feb391657072d1feb9aadf934e5efdf6493
Mercy Ma <mercyblitz@gmail.com>
2021-05-20 22:38:21 +0800

    Polish Code

---
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/rpc/dubbo/ZookeeperDubboServiceProviderBootstrap.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geektimes.projects.user.rpc.dubbo;
+
+import org.apache.dubbo.config.bootstrap.DubboBootstrap;
+import org.geektimes.projects.user.service.EchoService;
+import org.geektimes.projects.user.service.EchoServiceImpl;
+
+import static org.apache.dubbo.common.constants.CommonConstants.COMPOSITE_METADATA_STORAGE_TYPE;
+
+/**
+ * TODO Comment
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since TODO
+ */
+public class ZookeeperDubboServiceProviderBootstrap {
+
+    public static void main(String[] args) {
+        DubboBootstrap.getInstance()
+                .application("zookeeper-dubbo-provider", app -> app.metadata(COMPOSITE_METADATA_STORAGE_TYPE))
+                .registry(builder -> builder.address("127.0.0.1:2181")
+                                .protocol("zookeeper")
+//                        .parameter(REGISTRY_TYPE_KEY, SERVICE_REGISTRY_TYPE)
+                )
+                .protocol("dubbo", builder -> builder.port(-1).name("dubbo"))
+                .protocol("rest", builder -> builder.port(8081).name("rest").server("netty"))
+                .service("echo", builder -> builder.interfaceClass(EchoService.class).ref(new EchoServiceImpl()).protocolIds("dubbo,rest"))
+                .start()
+                .await();
+    }
+}
