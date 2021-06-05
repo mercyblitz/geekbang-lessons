11d95697cf24f9085375e21db0f09fc74092e1a2
Mercy Ma <mercyblitz@gmail.com>
2021-06-05 22:53:43 +0800

    Polish code

--- projects/stage-0/user-platform/spring-cloud-projects/spring-cloud-service-consumer/src/main/java/org/geektimes/projects/spring/cloud/service/consumer/ServiceConsumer.java
+++ projects/stage-0/user-platform/spring-cloud-projects/spring-cloud-service-consumer/src/main/java/org/geektimes/projects/spring/cloud/service/consumer/ServiceConsumer.java
@@ -18,14 +18,23 @@ package org.geektimes.projects.spring.cloud.service.consumer;
 
 import org.geektimes.projects.spring.cloud.service.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.SubscribableChannel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.Consumer;
 
 /**
  * 服务消费方应用引导类
@@ -37,10 +46,14 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @RestController
 @EnableFeignClients(basePackages = "org.geektimes.projects.spring.cloud.service")
+//@EnableBinding(MySink.class)
 public class ServiceConsumer {
 
     @Autowired
     private EchoService echoService;
+
+//    @Autowired
+//    private MySink mySink;
 
     @Autowired
     private CircuitBreakerFactory circuitBreakerFactory;
@@ -53,6 +66,37 @@ public class ServiceConsumer {
                 .run(() -> "Call : " + echoService.echo(message));
 
         return "Call : " + echoService.echo(message);
+    }
+
+//    @Bean
+//    public ApplicationRunner runner() {
+//        return args -> {
+//            // 耦合 Spring Message API
+//            SubscribableChannel subscribableChannel = mySink.input1();
+//            subscribableChannel.subscribe(message -> {
+//                System.out.println("SubscribableChannel 消息内容：" + message.getPayload());
+//            });
+//        };
+//    }
+
+//    @ServiceActivator(inputChannel = "input1") // 耦合 Spring Integration API
+//    public void onMessage(Message<?> message) {
+//        System.out.println("@ServiceActivator 消息内容：" + message.getPayload());
+//    }
+
+//    @StreamListener("input1")  // 耦合 Spring Cloud Stream API
+//    public void onStreamMessage(Message<?> message) {
+//        System.out.println("@StreamListener 消息内容：" + message.getPayload());
+//    }
+
+    // Spring Cloud Stream 3.0+ 函数接口方式
+    @Bean
+    public Consumer<String> message() {
+        // 可能与 Spring Cloud Stream 老版本的 @EnableBinding(MySink.class) 冲突
+        // 与 @StreamListener("input1")
+        return message -> {
+            System.out.println("Consumer 消息内容：" + message);
+        };
     }
 
     public static void main(String[] args) {
--- projects/stage-0/user-platform/spring-cloud-projects/spring-cloud-service-provider/src/main/java/org/geektimes/projects/spring/cloud/service/provider/ServiceProvider.java
+++ projects/stage-0/user-platform/spring-cloud-projects/spring-cloud-service-provider/src/main/java/org/geektimes/projects/spring/cloud/service/provider/ServiceProvider.java
@@ -16,13 +16,21 @@
  */
 package org.geektimes.projects.spring.cloud.service.provider;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +46,9 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+//@EnableCircuitBreaker
+@EnableBinding({MySource.class})
+@EnableScheduling
 public class ServiceProvider {
 
     public static void main(String[] args) {
@@ -51,6 +62,9 @@ public class ServiceProvider {
 
     private int port;
 
+    @Autowired
+    private MySource mySource;
+
     @PostConstruct
     public void init() {
         this.localIp = inetUtils.findFirstNonLoopbackHostInfo().getIpAddress();
@@ -62,9 +76,35 @@ public class ServiceProvider {
     }
 
 
+    @Bean
+    public ApplicationRunner runner() {
+        return args -> {
+            sendMessage();
+        };
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage() {
+        MessageChannel messageChannel = mySource.output1();
+        GenericMessage<String> message = new GenericMessage("Hello,World");
+        messageChannel.send(message);
+    }
+
+    @Scheduled(fixedRate = 3000)
+    public void sendMessage2() {
+        MessageChannel messageChannel = mySource.output2();
+        GenericMessage<String> message = new GenericMessage("2021");
+        messageChannel.send(message);
+    }
+
+    @CircuitBreaker(name = "echo", fallbackMethod = "fallback")
     @GetMapping("/echo/{message}")
     public String echo(@PathVariable String message) {
         return messageWithExtraInfo("[ECHO] : " + message);
+    }
+
+    public String fallback(String message, Throwable throwable) {
+        return "FALLBACK";
     }
 
 //    private String messageWithThreadInfo(String message) {
