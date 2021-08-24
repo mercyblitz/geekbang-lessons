5e666ffb9b7e552f5b8382c1f95234e1fe5fb3b5
Mercy Ma <mercyblitz@gmail.com>
2021-08-25 02:30:52 +0800

    Polish code

--- projects/stage-1/middleware-frameworks/my-cdi/src/main/java/org/geektimes/enterprise/inject/util/ProducerMethods.java
+++ projects/stage-1/middleware-frameworks/my-cdi/src/main/java/org/geektimes/enterprise/inject/util/ProducerMethods.java
@@ -16,21 +16,33 @@
  */
 package org.geektimes.enterprise.inject.util;
 
+import org.geektimes.enterprise.inject.standard.ManagedBean;
+import org.geektimes.enterprise.inject.standard.ProducerMethodBean;
+
 import javax.decorator.Decorator;
+import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Observes;
 import javax.enterprise.event.ObservesAsync;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.Specializes;
+import javax.enterprise.inject.spi.AnnotatedMethod;
 import javax.enterprise.inject.spi.DefinitionException;
 import javax.inject.Inject;
 import javax.interceptor.Interceptor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableSet;
+import static org.geektimes.commons.reflect.util.MemberUtils.isStatic;
+import static org.geektimes.commons.reflect.util.TypeUtils.asParameterizedType;
 import static org.geektimes.enterprise.inject.util.Producers.validateProducerRequiredAnnotation;
 
 /**
@@ -41,12 +53,72 @@ import static org.geektimes.enterprise.inject.util.Producers.validateProducerReq
  */
 public abstract class ProducerMethods {
 
+    public static Set<ProducerMethodBean> resolveProducerMethodBeans(ManagedBean<?> managedBean) {
+        Set<ProducerMethodBean> producerMethodBeans = new LinkedHashSet<>();
+        Set<AnnotatedMethod> methods = managedBean.getAnnotatedType().getMethods();
+        for (AnnotatedMethod method : methods) {
+            Method javaMethod = method.getJavaMember();
+            if (isProduceMethod(javaMethod)) {
+                ProducerMethodBean producerMethodBean = new ProducerMethodBean(javaMethod);
+                producerMethodBeans.add(producerMethodBean);
+            }
+        }
+        return unmodifiableSet(producerMethodBeans);
+    }
+
+    /**
+     * A producer method must be a default-access, public, protected or private,
+     * non-abstract method of a managed bean class.
+     * A producer method may be either static or non-static.
+     *
+     * @param method {@link Method}
+     * @return <code>true</code> if {@link Method} is producer
+     */
+    private static boolean isProduceMethod(Method method) {
+        return !isStatic(method) && method.isAnnotationPresent(Produces.class);
+    }
+
     /**
      * @param producerMethod {@link Method Producer Method}
      * @throws DefinitionException if {@link Method Producer Method} does not annotate {@link Produces @Produces}
      */
     public static void validateProducerMethodProduces(Method producerMethod) throws DefinitionException {
         validateProducerRequiredAnnotation(producerMethod, Produces.class);
+    }
+
+    /**
+     * <ul>
+     *     <li>
+     *         If the producer method return type is a parameterized type, it must specify an actual type parameter or
+     *         type variable for each type parameter.
+     *     </li>
+     *     <li>
+     *         If a producer method return type contains a wildcard type parameter or is an array type whose component
+     *         type contains a wildcard type parameter, the container automatically detects the problem and treats it as a definition error.
+     *     </li>
+     *     <li>
+     *         If the producer method return type is a parameterized type with a type variable, it must have scope
+     *         {@link Dependent @Dependent}.
+     *     </li>
+     *     <li>
+     *         If a producer method with a parameterized return type with a type variable declares any scope other than
+     *         {@link Dependent @Dependent}, the container automatically detects the problem and
+     *         treats it as a definition error.
+     *     </li>
+     *     <li>
+     *         If a producer method return type is a type variable or an array type whose component type is a type
+     *         variable the container automatically detects the problem and treats it as a definition error.
+     *     </li>
+     * </ul>
+     *
+     * @param producerMethod the method annotated {@link Produces @Produces}
+     */
+    public static void validateProducerMethodReturnType(Method producerMethod) {
+        Type returnType = producerMethod.getGenericReturnType();
+        ParameterizedType returnParameterizedType = asParameterizedType(returnType);
+        if (returnParameterizedType != null) {
+            returnParameterizedType.getActualTypeArguments();
+        }
     }
 
     /**
@@ -85,18 +157,8 @@ public abstract class ProducerMethods {
      */
     public static void validateProducerMethodDeclaringClass(Method producerMethod) throws DefinitionException {
         Class<?> declaringType = producerMethod.getDeclaringClass();
-        validateDeclaringType(declaringType, Interceptor.class);
-        validateDeclaringType(declaringType, Decorator.class);
-    }
-
-    private static void validateDeclaringType(Class<?> declaringType, Class<? extends Annotation> annotationType) throws DefinitionException {
-        if (declaringType.isAnnotationPresent(annotationType)) {
-            String errorMessage = format("The Producer Methods' declaring type[type : %s] must not annotate @%s",
-                    declaringType.getName(),
-                    annotationType.getName()
-            );
-            throw new DefinitionException(errorMessage);
-        }
+        validateForbiddenAnnotation(declaringType, Interceptor.class);
+        validateForbiddenAnnotation(declaringType, Decorator.class);
     }
 
     public static void validateProducerMethodParameters(Method producerMethod) {
@@ -118,15 +180,25 @@ public abstract class ProducerMethods {
      *                             has a parameter annotated {@link ObservesAsync @ObservesAsync},
      *                             the container automatically detects the problem and treats it as a definition error.
      */
-    private static void validateAnnotatedParameter(Parameter parameter, int index, Method producerMethod) throws DefinitionException {
+    public static void validateAnnotatedParameter(Parameter parameter, int index, Method producerMethod) throws DefinitionException {
         if (producerMethod.isAnnotationPresent(Inject.class)) {
-            validateParameterAnnotation(parameter, index, Disposes.class);
-            validateParameterAnnotation(parameter, index, Observes.class);
-            validateParameterAnnotation(parameter, index, ObservesAsync.class);
+            validateForbiddenAnnotation(parameter, index, Disposes.class);
+            validateForbiddenAnnotation(parameter, index, Observes.class);
+            validateForbiddenAnnotation(parameter, index, ObservesAsync.class);
         }
     }
 
-    private static void validateParameterAnnotation(Parameter parameter,
+    private static void validateForbiddenAnnotation(Class<?> declaringType, Class<? extends Annotation> annotationType) throws DefinitionException {
+        if (declaringType.isAnnotationPresent(annotationType)) {
+            String errorMessage = format("The Producer Methods' declaring type[type : %s] must not annotate @%s",
+                    declaringType.getName(),
+                    annotationType.getName()
+            );
+            throw new DefinitionException(errorMessage);
+        }
+    }
+
+    private static void validateForbiddenAnnotation(Parameter parameter,
                                                     int index,
                                                     Class<? extends Annotation> annotationType) throws DefinitionException {
         if (parameter.isAnnotationPresent(annotationType)) {
